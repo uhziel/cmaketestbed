579cefd638f8dd58580674f5792a5031d09a63f2
Lei Zhu <uhziel@gmail.com>
2012-09-01 23:03:58 +0800

    calc can do simply +, -, *, /.

--- src/calc/main.c
+++ src/calc/main.c
@@ -2,8 +2,45 @@
 
 #include "calc.h"
 
+#define BUF_SIZE 1024
+
+char cmd_line[BUF_SIZE];
+
+int calc(int lhs, char op, int rhs)
+{
+    switch (op)
+    {
+        case '+':
+            return add(lhs, rhs);
+        case '-':
+            return sub(lhs, rhs);
+        case '*':
+            return multi(lhs, rhs);
+        case '/':
+            return div(lhs, rhs);
+        default:
+            return 0;
+    }
+}
+
 int main()
 {
-    printf("1 + 2 = %d.\n", add(1, 2));
+    int lhs = 0;
+    char op = '\0';
+    int rhs = 0;;
+
+    printf("(calc) ");
+    while (gets(cmd_line))
+    {
+        lhs = 0;
+        op = '\0';
+        rhs = 0;
+
+        sscanf(cmd_line, "%d %c %d\n", &lhs, &op, &rhs);
+        printf("%d\n", calc(lhs, op, rhs));
+
+        printf("(calc) ");
+    }
+    printf("\n");
     return 0;
 }
