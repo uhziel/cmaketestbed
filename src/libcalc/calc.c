b363b97cbacc7c5679a6951c5ff8a36d093ba597
Lei Zhu <uhziel@gmail.com>
2012-09-01 18:06:09 +0800

    add libcalc.

---
+++ src/libcalc/calc.c
@@ -0,0 +1,21 @@
+#include "calc.h"
+
+int add(int lhs, int rhs)
+{
+    return lhs + rhs;
+}
+
+int sub(int lhs, int rhs)
+{
+    return lhs + rhs;
+}
+
+int multi(int lhs, int rhs)
+{
+    return lhs * rhs;
+}
+
+int div(int lhs, int rhs)
+{
+    return lhs / rhs;
+}
---
+++ src/libcalc/calc.h
@@ -0,0 +1,9 @@
+#ifndef __CALC_H__
+#define __CALC_H__
+
+int add(int lhs, int rhs);
+int sub(int lhs, int rhs);
+int multi(int lhs, int rhs);
+int div(int lhs, int rhs);
+
+#endif /* __CALC_H__ */
